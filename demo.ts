5a17e5d1afb36815f4b9fdda88a4282e0529a43c
Zhivko Shopov <zhishopov@gmail.com>
2025-08-21 11:42:52 +0100

    Practice basic data types

---
+++ demo.ts
@@ -0,0 +1,9 @@
+let firstName: string = "Josh";
+let lastName: string = "Hermano";
+let age: number = 25;
+let hasGraduated: boolean = true;
+
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(hasGraduated);
