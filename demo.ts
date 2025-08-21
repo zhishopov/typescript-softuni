be8ffe0c9bb22519269296bce43f832fae7f515a
Zhivko Shopov <zhishopov@gmail.com>
2025-08-21 11:53:01 +0100

    Practice more basic data types

--- demo.ts
+++ demo.ts
@@ -2,8 +2,13 @@ let firstName: string = "Josh";
 let lastName: string = "Hermano";
 let age: number = 25;
 let hasGraduated: boolean = true;
+let skills: string[] = ["JS", "HTML", "TypeScript"];
+let certificateInfo: [string, number, boolean] = ["MySQL", 2025, true];
+let strNumArr: (string | number)[] = [2, "test", 4];
 
 console.log(firstName);
 console.log(lastName);
 console.log(age);
 console.log(hasGraduated);
+console.log(skills);
+console.log(certificateInfo);
